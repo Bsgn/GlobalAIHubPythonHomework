9dec36f134bf07a1480938565f9312b547bbb0d5
Bsgn <55917176+Bsgn@users.noreply.github.com>
2020-12-22 00:22:40 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,11 @@
+val1 = input("Enter 1st value : ") 
+val2 = input("Enter 2nd value : ")
+val3 = input("Enter 3rd value : ")  #Taking values from user
+val4 = input("Enter 4th value : ")
+val5 = input("Enter 5th value : ")
+
+print(f"1st value : {val1}, type : {type(val1)}")
+print(f"2nd value : {val2}, type : {type(val2)}")
+print(f"3rd value : {val3}, type : {type(val3)}")   #Printing values and type of values
+print("4th value : {}, type : {}".format(val4, type(val4)))
+print("5th value : {}, type : {}".format(val5, type(val5)))
